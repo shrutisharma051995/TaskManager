694bf16c7b359556fdcfa48f4aac7c19cd928d9a
shrutisharma051995 <imshruti51@gmail.com>
2017-11-09 17:32:26 +0530

    completed app

---
+++ app/src/main/java/com/sh/taskmanagementapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sh.taskmanagementapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    TextView tv2 = (TextView)findViewById(R.id.tv2);
+
+
+}
---
+++ app/src/main/res/MyTask.java
@@ -0,0 +1,13 @@
+/**
+ * Created by SAMSUNG on 11/9/2017.
+ */
+public class MyTask
+{
+    String title;
+    String description;
+    Date deadlineDateTime;
+    boolean isComplete;
+
+}
+
+
